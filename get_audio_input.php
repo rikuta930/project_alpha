2fdfe25d457e4ecbd8f046b87b16b291c71a87be
nyumchan <nyumsakura.0316@gmail.com>
2020-09-05 02:10:42 +0900

    音声入力画面かり

--- get_audio_input.php
+++ get_audio_input.php
@@ -2,12 +2,18 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>音声入力画面</title>
   </head>
   <body>
-    <h1>音声入力を受け取る</h1>
-    <form class="" action="index.html" method="post">
-
-    </form>
+    <h1>音声入力</h1>
+    <input type="button" value="音声入力する" onmousedown="this.value='入力中…'" onmouseup="this.value='音声入力する'">
+    <br>
+      <input type="radio" name="happy" value="">happy
+      <input type="radio" name="sad" value="">sad
+      <input type="radio" name="man" value="">男性
+      <input type="radio" name="woman" value="">女性
+      <input type="radio" name="private" value="">秘密
+    <br>
+    <a href="#" class="submit">聞いてもらう</a>
   </body>
 </html>

673c5cc914ba07b7aa4756c2f759b5334a9d73d6
nyumchan <nyumsakura.0316@gmail.com>
2020-09-04 21:42:33 +0900

    初めてのコミット
    頑張ります

---
+++ get_audio_input.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>音声入力を受け取る</h1>
+    <form class="" action="index.html" method="post">
+
+    </form>
+  </body>
+</html>

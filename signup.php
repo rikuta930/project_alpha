3f6074821018f9f05a64234dc47f3ea799e576e9
Hannah-86 <hana86.shinee@gmail.com>
2020-09-04 23:03:09 +0900

    signinとsignupの簡易的なマークアップを作成

---
+++ signup.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sign up</title>
+  <link rel="stylesheet" href="css/sign.css">
+</head>
+<body>
+  <div class="main-container">
+    <h1>新規作成</h1>
+    <div class="sign-form">
+      <label>ユーザー名<input type="text"></label>
+      <label>メールアドレス<input type="text"></label>
+      <label>パスワード<input type="text"></label>
+      <label>パスワード(確認用)<input type="text"></label>
+      <button class="sign-btn">新規作成</button>
+    </div>
+  </div>
+</body>
+</html>

087befbb5692900a589c2b41989fe125e71f6dd2
tani <s1922067@musashino-u.ac.jp>
2020-09-05 16:18:17 +0900

    regform.htmlをsignup.phpに名前を変更。
    元々あったsignup.phpはregform.htmlに内容を移動させたため、消去しました。

--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,12 +10,27 @@
   <div class="main-container">
     <h1>新規作成</h1>
     <div class="sign-form">
-      <label>ユーザー名<input type="text"></label>
-      <label>メールアドレス<input type="text"></label>
-      <label>パスワード<input type="text"></label>
-      <label>パスワード(確認用)<input type="text"></label>
-      <button class="sign-btn">新規作成</button>
+      <form method="POST" action="regist.php">
+      ユーザ名:
+        <label>
+          <input type="text" name="user_name" size="40">
+        </label><br>
+        email:
+        <label>
+          <input type="text" name="email" size="40">
+        </label><br>
+        password:
+        <label>
+          <input type="password" name="password_1" size="40">
+        </label><br>
+        password:
+        <label>
+          <input type="password" name="password_2" size="40">
+        </label>(確認)<br>
+        <input type="submit" value="登録">
+      </form>
     </div>
   </div>
+
 </body>
 </html>

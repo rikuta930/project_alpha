860777744084be991ec089292b004647235f2b90
Hannah-86 <hana86.shinee@gmail.com>
2020-09-05 16:16:52 +0900

    プロフィールの簡易的なマークアップを作成

---
+++ profile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Profile</title>
+</head>
+<body>
+  <div class="main-container">
+    <h1>プロフィール編集</h1>
+    <div class="form">
+      <label>名前<input type="text"></label>
+      <label>自己紹介<textarea name="introduction"></textarea></label>
+    </div>
+  </div>
+</body>
+</html>

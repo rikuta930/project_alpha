793fe5a04aeb4719f1bc0bb08e733a1c8b1856cf
nyumchan <nyumsakura.0316@gmail.com>
2020-09-06 02:14:43 +0900

    timelineの更新

--- timeline.php
+++ timeline.php
@@ -1,10 +1,47 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>timeline</title>
-  </head>
-  <body>
-    
-  </body>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>timeline</title>
+  <link rel="stylesheet" href="css/timeline.css">
+  <link href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" rel="stylesheet">
+</head>
+<body>
+  <header>
+    ここにヘッダー
+  </header>
+  <div class="main-container">
+    <img src="image/blue.png" alt="" width="50" height="50">
+    <a href="profile.php">
+      Taro
+    </a>
+    <br>
+  <audio controls>
+     <source src="#" type="audio/mp3">
+     <source src="#" type="audio/ogg">
+     <source src="#" type="audio/wav">
+     <p>（audio要素に非対応なブラウザ向けの表示）</p>
+  </audio>
+    <br>
+    <p id="free-word">
+      #嬉しい #happy
+    </p>
+
+  <button>
+    <i class="fas fa-assistive-listening-systems">
+    </i>
+  </button>
+  <p id="count">0</p>
+  </div>
+
+  <footer>
+    <a href="form.php">
+      <i class="fas fa-home"></i>
+    </a>
+    <a href="#">
+      <i class="fas fa-microphone"></i>
+    </a>
+  </footer>
+</body>
 </html>

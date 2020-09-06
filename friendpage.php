319b07ccb921272e90ac17f92b1c1aaf7268f10b
Hannah-86 <hana86.shinee@gmail.com>
2020-09-07 05:33:41 +0900

    friendpageのスタイリング

--- friendpage.php
+++ friendpage.php
@@ -1,60 +1,69 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mypage</title>
-    <link rel="stylesheet" href="css/reboot.min.css">
-    <link rel="stylesheet" href="css/mypage.css">
+    <link rel="stylesheet" href="./css/reboot.min.css" />
+    <link rel="stylesheet" href="./css/friendpage.css" />
   </head>
   <body>
-<div class="main-container">
-  <img src="icon/girl.png" alt="" width="100" height="100">
-   <div class="follow">
-     <p>フォロー</p>
-     <p id="follow">5</p>
-   </div>
-   <div class="follower">
-     <p>フォロワー</p>
-     <p id="follow">5</p>
-   </div>
-  <h2>Hana</h2>
-  <p id="id">ID:aaaaaaa</p>
-  <p id="profile">自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介</p>
-  <div class="FollowButton">
-　　　　　<button id="follow-button" class="open" onClick="onClickFollow()">
-　　　　　　　　　　<span id="follow-no" style="display: block">フォローする</span>　　　
-　　　　　　　　　　<span id="follow-yes" style="display: none">フォロー中</span>
-　　　　　</button>
-  </div>
- </div>
-<hr>
-
-  <div class="secound-container">
-    <img src="icon/girl.png" alt="" width="50" height="50">
-    <p>Hana</p>
-    <br>
-  <audio controls>
-     <source src="#" type="audio/mp3">
-     <source src="#" type="audio/ogg">
-     <source src="#" type="audio/wav">
-     <p>（audio要素に非対応なブラウザ向けの表示）</p>
-  </audio>
-    <br>
-    <p id="free-word">
-      #嬉しい #happy
-    </p>
-​
-  <button>
-    <img src="icon/ear_black.png" alt="" width="50" height="50">
-  </button>
-  <p id="count">15</p>
-  <hr>
-  </div>
+    <header class="header">
+      <img src="./icon/logo.png" class="header__icon" />
+      <div class="header__title">サービス名</div>
+      <img src="./icon/search.png" class="header__search">
+    </header>
+    <div class="main-container">
+      <div class="profile">
+        <div class="profile__left">
+          <img src="./icon/girl.png" alt="" width="100" height="100" class="profile__left__icon"/>
+          <div class="profile__left__number">
+            <div class="profile__left__follow">
+              <h2 class="profile__follow-title">フォロー</h2>
+              <p class="profile__follow-number" id="follow">5</p>
+            </div>
+            <div class="profile__left__follow">
+              <h2 class="profile__follow-title">フォロワー</h2>
+              <p class="profile__follow-number" id="follow">5</p>
+            </div>
+          </div>
+        </div>
+        <div class="profile__right">
+          <h2 class="profile__right__name">Hana</h2>
+          <div id="id" class="profile__right__id">aaaaaaa</div>
+          <div id="divrofile" class="profile__right__introduction">
+            自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介自己紹介
+          </div>
+          <div class="profile__btn-wrapper">
+            <button class="profile__btn">フォロー</button>
+          </div>
+        </div>
+      </div>
+      <div class="list">
+        <img src="icon/girl.png" alt="" width="50" height="50" />
+        <p>Hana</p>
+        <br />
+        <audio controls>
+          <source src="#" type="audio/mp3" />
+          <source src="#" type="audio/ogg" />
+          <source src="#" type="audio/wav" />
+          <p>（audio要素に非対応なブラウザ向けの表示）</p>
+        </audio>
+        <br />
+        <p id="free-word">#嬉しい #happy</p>
+        ​
+        <button>
+          <img src="icon/ear_black.png" alt="" width="50" height="50" />
+        </button>
+        <p id="count">15</p>
+      </div>
+    </div>
     <a href="timeline.php">
-      <img src="icon/timeline.png" alt="">
+      <img src="./icon/timeline.png" alt="" />
     </a>
     <a href="form.php">
-      <img src="icon/mic.png" alt="" >
+      <img src="./icon/mic.png" alt="" />
     </a>
-</body>
+  </body>
 </html>
+
